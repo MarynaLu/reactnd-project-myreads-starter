8652853e556e7474393d4d28d9064cb001ae0463
Maryna-Lus <m.lus@smartassistant.com>
2018-07-17 21:25:58 +0200

    Added changing shelves

---
+++ src/Book.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+//import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import escapeRegExp from 'escape-string-regexp'
+//import sortBy from 'sort-by'
+
+class Book extends Component {
+	state = {
+		shelf: ''
+	}
+
+	
+	render () {
+		const { moveBook } = this.props
+
+		return (
+			 /* Currently reading section*/
+            <div className="list-books-content">
+              <div>
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">Currently Reading</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                      
+                      {this.props.shelf.map((book) => (
+                      		<li key={book.id}>
+								<div className="book">
+									<div className="book-top">
+										<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+											<div className="book-shelf-changer">
+												<select onChange={(event) => moveBook(book.id, event.target.value)}>
+					                                <option value="move" disabled>Move to...</option>
+					                                <option value="currentlyReading">Currently Reading</option>
+					                                <option value="wantToRead">Want to Read</option>
+					                                <option value="read">Read</option>
+					                                <option value="none">None</option>
+                              					</select>
+											</div>
+										</div>
+										<div className="book-title">{book.title}</div>
+										<div className="book-authors">{book.authors}</div>
+									</div>
+                      		</li>
+						))}
+                    </ol>
+                  </div>
+                </div>
+              </div>
+            </div>
+		)
+	}
+}
+
+export default Book
---
+++ src/CreateShelves.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react'
+//import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import escapeRegExp from 'escape-string-regexp'
+import Book from './Book'
+//import sortBy from 'sort-by'
+
+class CreateShelves extends Component {
+	state = {
+		shelf: ''
+	}
+
+
+
+  	render() {
+  		 const { moveBook } = this.props
+
+    return (
+      <div className="app">
+        {this.props.searchPage ? (
+          <div className="search-books">
+            <div className="search-books-bar">
+              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+              <div className="search-books-input-wrapper">
+                {/*
+                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                  You can find these search terms here:
+                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                  you don't find a specific author or title. Every search is limited by search terms.
+                */}
+                <input type="text" placeholder="Search by title or author"/>
+
+              </div>
+            </div>
+            
+            <div className="search-books-results">
+              <ol className="books-grid"></ol>
+            </div>
+          </div>
+        ) : (
+          <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+			
+		<Book 
+			shelf = {this.props.allBooks.filter(book => book.shelf === "currentlyReading")}
+			moveBook={moveBook}
+		/>	
+         
+        <Book
+        	shelf={this.props.allBooks.filter(book => book.shelf === "wantToRead")}
+        	moveBook={moveBook}
+        />
+
+        <Book
+        	shelf={this.props.allBooks.filter(book => book.shelf === "read")}
+        	moveBook={moveBook}
+        />
+
+          {/*Open search section*/}
+            <div className="open-search">
+              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+            </div>
+          </div>
+        )}
+      </div>
+    )
+  }
+}
+
+export default CreateShelves
